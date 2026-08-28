b063e958127fe971f1416c0f88c04b6155d8e85f
Yury Babrounik <bvr0909@gmail.com>
2026-08-28 09:13:01 +0300

    task 3: use `.get()` for conn,host,port also; clarify

--- scripts/python_datatypes/task3_api_parse.py
+++ scripts/python_datatypes/task3_api_parse.py
@@ -6,13 +6,17 @@ db_config = {
     },
 }
 
+# defined as consts to be config-like defaults; maybe ommit _DEFAULT
+HOST_DEFAULT = "production-db.internal"
+PORT_DEFAULT = 5432
 SSL_MODE_DEFAULT = "verify-full"
 
-db_connection = db_config["connection"]
-
-# probably for later use
-host = db_connection["host"]
-port = db_connection["port"]
+# not sure how to treat config dict based on task condition,
+# whether it is strictly defined or serves as a reference
+# .get() with defaults, just in case
+db_connection = db_config.get("connection", {})
+host = db_connection.get("host", HOST_DEFAULT)
+port = db_connection.get("port", PORT_DEFAULT)
 
 # use empty dict as fallback in first .get() call, so chained 2nd .get()
 # safely fallbacks to SSL_MODE_DEFAULT when 'ssl_settings' is missing
