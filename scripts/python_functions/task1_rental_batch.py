f8d6979a6ef44e4aa7e8bd864a3910d21448da07
Yury Babrounik <bvr0909@gmail.com>
2026-09-01 02:11:09 +0300

    task 1: unpack in `print_rental_batch()`

--- scripts/python_functions/task1_rental_batch.py
+++ scripts/python_functions/task1_rental_batch.py
@@ -32,22 +32,21 @@ def print_rental_batch(data: list[tuple[str, int, float, float]]) -> None:
     """
     print("=== RENTAL BATCH REPORT ===")
 
-    # keep just indexing here, cause tuple is fixed and unpacking adds noise
     for i, batch in enumerate(data, start=1):
-        title = batch[0]
+        title, quantity, rental_rate, discount = batch
         # to show named vs positional args usage
         match i:
             case 0:
                 final_sum, is_limit_exceeded = calculate_rental_batch(
-                    quantity=batch[1],
-                    rental_rate=batch[2],
-                    discount=batch[3],
+                    discount=discount,
+                    quantity=quantity,
+                    rental_rate=rental_rate,
                 )
             case _:
                 final_sum, is_limit_exceeded = calculate_rental_batch(
-                    batch[1],
-                    batch[2],
-                    batch[3],
+                    quantity,
+                    rental_rate,
+                    discount,
                 )
 
         print(
